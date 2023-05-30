27dc5fb6dfe844dffe85250a6924717a97a945a7
Érico <eriicoalan@gmail.com>
2023-05-29 23:17:32 -0300

    Ajustes

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+// $('#accordionFlushExample .collapse').on('show.bs.collapse', function (e) {
+//     var actives = $('#accordionFlushExample').find('.in, .collapsing');
+//     actives.each( function (index, element) {
+//         $(element).collapse('hide');
+//     })
+// })
