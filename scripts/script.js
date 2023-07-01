70acb832bfefa0fae6566d391b8917ddb1603a73
ericoasferreira <Zangetsu13@>
2023-07-01 17:50:45 -0300

    Start active first

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
   const buttons = document.querySelectorAll(".custom-accordion-button");
   const contents = document.querySelectorAll(".custom-accordion-content");
 
+  // Add active class to the first button
+  buttons[0].classList.add("active");
+
+  // Show the corresponding content for the first button
+  contents[0].style.display = "block";
+
   buttons.forEach((button, index) => {
     button.addEventListener("click", function() {
       // Remove the active class from all buttons
